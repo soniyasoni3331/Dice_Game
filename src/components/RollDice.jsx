50a4e543d348b4fc27a3b34df117924df3bf6ae1
Soniya <soniyasoni3331@gmail.com>
2024-06-27 19:18:48 +0530

    final update

--- src/components/RollDice.jsx
+++ src/components/RollDice.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import dice1 from '../assets/dice_1.png';
+import dice2 from '../assets/dice_2.png';
+import dice3 from '../assets/dice_3.png';
+import dice4 from '../assets/dice_4.png';
+import dice5 from '../assets/dice_5.png';
+import dice6 from '../assets/dice_6.png';
+
+const diceImages = [null, dice1, dice2, dice3, dice4, dice5, dice6];
 
 function RollDice({RollDice, currentDice}) {
 
   return (
     <div onClick={RollDice}>
-        <img src={`/src/assets/dice_${currentDice}.png`} alt="Dice 1" />
+        <img src={diceImages[currentDice]} alt={`Dice ${currentDice}`} />
     </div>
   )
 }
